626e22e90461297bd5ea4e28f6fea468b4337f4e
Mikey <mikey@mikeys.biz>
2012-08-10 14:58:55 +1000

    Changed namespace

--- RapidGator_Premium_Downloader.user.js
+++ RapidGator_Premium_Downloader.user.js
@@ -2,18 +2,24 @@
 // @name        RapidGator Premium Downloader
 // @namespace   https://github.com/ohec/RapidGator-Premium-Downloader
 // @description Downloads from RapidGator account
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// @downloadURL https://github.com/ohec/RapidGator-Premium-Downloader/raw/master/RapidGator_Premium_Downloader.user.js
 // @include     http://rapidgator.net/file/*
 // @include     http://www.rapidgator.net/file/*
+// @include     */fhn.html
+// @include     */s.html
 // @version     0.1
 // ==/UserScript==
+// Add jQuery
+(function(){
+	if (typeof jQuery == 'undefined') {
+		var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
+			GM_JQ = document.createElement('script');
 
-var $;
-(function () {
-	if (typeof unsafeWindow.jQuery == 'undefined') {
-		var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement, GM_JQ = document.createElement('script');
 		GM_JQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js';
 		GM_JQ.type = 'text/javascript';
 		GM_JQ.async = true;
+
 		GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
 	}
 	GM_wait();
@@ -21,32 +27,33 @@ var $;
 
 // Check if jQuery's loaded
 function GM_wait() {
-	if (typeof unsafeWindow.jQuery == 'undefined') {
+	if (typeof jQuery == 'undefined') {
+		console.log('undefined');
 		window.setTimeout(GM_wait, 100);
 	} else {
-		$ = unsafeWindow.jQuery.noConflict(true);
+		console.log('defined');
+		$ = jQuery.noConflict(true);
 		letsJQuery();
 	}
 }
 
+// The Code
 function letsJQuery() {
-//console.log($('.btm > p > a').attr('href'));
-	if ($('.table_header').text().trim() != 'File not found') {
-		//$('.btm > p > a').text().trim()
-		$(document).attr('title', 'Downloading...');
-		//$('body').html('<pre style="margin-left:30px;margin-top:20px;">Downloading "'   + '" ...</pre>');
-		//unsafeWindow.location.href = $('.btm > p > a').attr('href');
-		$('.btm > p > a').click();
-
-		//setTimeout(function () { window.close(); }, 4000);
-		/*var link = $('.btm > p > a');
-
-		if ($('.btm > p > a') != null) {
-
-
-
-
-
-		}*/
+	var i, x;
+	for (i = 0; x = document.styleSheets[i]; ++i) {
+		x.disabled = true;
 	}
+	if(document.title == "File not found") {
+		console.log('File not found - Closing');
+	} else {
+		var link2 = $('.btm > p > a');
+		var url = $.trim(link2.attr('href'));
+
+		$('body').html('<pre style="margin-left:30px;margin-top:20px;">Downloading "' + $(document).attr('title') + '" ...</pre>');
+
+		link2.click();
+		//window.location.href = url;
+	}
+
+	setTimeout(function() { window.close(); }, 10000);
 }
