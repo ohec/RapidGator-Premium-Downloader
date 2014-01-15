5b67df7a7ebd5fb02de42b9e08818b0b2fa366e2
Mikey Cawley <mikey@mikeys.biz>
2014-01-15 11:35:23 +1100

    Formatted it

--- RapidGator_Premium_Downloader.user.js
+++ RapidGator_Premium_Downloader.user.js
@@ -8,54 +8,54 @@
 // @include     http://www.rapidgator.net/file/*
 // ==/UserScript==
 (function () {
-	var gm_plugin = function () {
-		var options = {
-			debug:    false,
-			openLink: true,  // Open by clicking the link
-			openUrl:  false   // Open by setting the window href
-		};
+    var gm_plugin = function () {
+        var options = {
+            debug: false,
+            openLink: true,  // Open by clicking the link
+            openUrl: false   // Open by setting the window href
+        };
 
-		return {
-			/**
-			 * Do the click
-			 */
-			init: function () {
-				if (document.title == 'File not found') {
-					this.log('File not found - Closing');
-					unsafeWindow.close();
-				} else {
-					var downloadLinks;
-					downloadLinks = document.evaluate('//div[@class="btm"]/p/a', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-					for (var i = 0; i < downloadLinks.snapshotLength; i++) {
-						var link = downloadLinks.snapshotItem(i);
-						var url = link.href;
+        return {
+            /**
+             * Do the click
+             */
+            init: function () {
+                if (document.title == 'File not found') {
+                    this.log('File not found - Closing');
+                    unsafeWindow.close();
+                } else {
+                    var downloadLinks;
+                    downloadLinks = document.evaluate('//div[@class="btm"]/p/a', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+                    for (var i = 0; i < downloadLinks.snapshotLength; i++) {
+                        var link = downloadLinks.snapshotItem(i);
+                        var url = link.href;
 
-						if(options.openUrl == 1) {
-							unsafeWindow.location.href = url;
-						} else if(options.openLink == 1) {
-							link.click();
-						} else {
-							this.log('Opening ' + url);
-						}
-						//unsafeWindow.location.href = link.href;
-						//link.click();
-					}
-				}
-			},
+                        if (options.openUrl == 1) {
+                            unsafeWindow.location.href = url;
+                        } else if (options.openLink == 1) {
+                            link.click();
+                        } else {
+                            this.log('Opening ' + url);
+                        }
+                        //unsafeWindow.location.href = link.href;
+                        //link.click();
+                    }
+                }
+            },
 
-			/**
-			 * Log message
-			 *
-			 * @param message
-			 */
-			log: function (message) {
-				if (options.debug > 0) {
-					console.log(message);
-				}
-			}
-		};
-	}
+            /**
+             * Log message
+             *
+             * @param message
+             */
+            log: function (message) {
+                if (options.debug > 0) {
+                    console.log(message);
+                }
+            }
+        };
+    }
 
-	var gm = new gm_plugin();
-	gm.init();
+    var gm = new gm_plugin();
+    gm.init();
 })();
